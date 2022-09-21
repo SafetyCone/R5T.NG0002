4dd0087b05308888363d3d8dc5ba1dc8cc84f9f9
David Coats <david@getsafetycone.com>
2022-09-21 11:53:19 -0700

    Initial commit.

---
+++ source/R5T.NG0002/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.NG0002
+{
+	/// <summary>
+	/// Microsoft.Extensions.DependencyInjection NuGet package selector library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
